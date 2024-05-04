dbc241fdcc7936e87a841f571c4c1aacd81fe31a
Denis Osipov <90373317+ddenyy@users.noreply.github.com>
2024-05-04 17:32:24 +0300

    create 2 func for valid order

--- src/Order/order.py
+++ src/Order/order.py
@@ -25,6 +25,15 @@ class Order:
             result.append(product.name)
         return ', '.join(result)
     
+    @property
+    def is_valid(self) -> bool:
+        return not self.is_any_none and len(self.products) != 0
+    
+    @property
+    def is_any_none(self) -> bool:
+        return self.shop_id is None or self.area is None or \
+            self.products is None or \
+            self.price is None
 
     @property
     def is_empty_products(self) -> bool:
