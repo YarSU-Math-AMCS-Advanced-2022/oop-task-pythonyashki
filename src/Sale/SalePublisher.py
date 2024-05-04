afc86910e6c2d5d28b2e3cc0f951f7f474c59e7c
Denis Osipov <90373317+ddenyy@users.noreply.github.com>
2024-05-04 20:34:35 +0300

    refactor-sale-publisher
    added id for owner_shop

--- src/Sale/SalePublisher.py
+++ src/Sale/SalePublisher.py
@@ -1,13 +1,16 @@
 from Sale import Sale
 from typing import Optional
 from abc import ABC, abstractmethod
+from enum import Enum
+from utils.enums import EventsSales
 
 class SubscriberSale(ABC):
     @abstractmethod
     def update(event: str): pass
-        
+
 
 class SalePublisher:
+    _owner_shop_id: str
     sales: list
     subscribers: list[SubscriberSale]
     
@@ -17,8 +20,10 @@ class SalePublisher:
     def unsubscribe(self, client_id: str):
         self.subscribe.remove(client_id)
 
-    def notify_subs(self, event):
+    def notify_subs(self, event: EventsSales, message: str):
         for i in len(self.subscribers):
-            self.subscribers[i].update(event)
+            self.subscribers[i].update(event, message)
+
+
     
         
--- src/utils/enums.py
+++ src/utils/enums.py
@@ -7,3 +7,7 @@ class Area(Enum):
     a = 1
     b = 2
     c = 3
+
+class EventsSales(Enum):
+    newSale: 1
+    deleteSale: 2
