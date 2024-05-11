a8fdf46616691a9efca25d6a4800776b8dbe1708
Kozlov Arseny <jetpilotat1il@gmail.com>
2024-05-11 18:23:27 +0300

    add product class

--- src/Product/product.py
+++ src/Product/product.py
@@ -1,8 +1,27 @@
-
 from dataclasses import dataclass
-
+from typing import Optional
+#id: number
+#name: string
+#isHasSale: boolean
+#price: number:
+#isHightAge: boolean
+#count: number
+#weight: number
 
 @dataclass
 class Product:
-    def hello():
-        print("hello_world")
+    """
+    """
+    id: Optional[int] = None
+    name: Optional[str] = None
+    price: Optional[int] = None
+    isHasSale: Optional[bool] = None
+    isHightAge: Optional[bool] = None
+    count: Optional[int] = None
+    weight: Optional[int] = None
+
+    def __str__(self) -> str:
+        return f'Id: {self.id}, Name: {self.name},'\
+               f'Price: {self.price}, Has sale: {self.isHasSale}'\
+               f'Hight age: {self.isHightAge}, Count: {self.count}'\
+               f'Weight: {self.weight}'
